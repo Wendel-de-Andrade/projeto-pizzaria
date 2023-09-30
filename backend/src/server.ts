ba441741ffad7cf6ecbf612bd3d888d991edb009
Wendel de Andrade <shindagami@gmail.com>
2023-09-30 14:44:13 -0300

    Teste

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,4 +23,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     })
 })
 
-app.listen(3333, () => console.log("Server Online!"))
+app.listen(3333, () => console.log("Server Online!!"))
