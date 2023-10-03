7b55253b8bd2b58a28a3a834e9541a0a7abd20cb
Wendel de Andrade <shindagami@gmail.com>
2023-10-02 22:19:59 -0300

    Criado serviço para adicionar categorias e listar todas elas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,12 +2,20 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
+// Rotas de Usuário
 router.post("/users", new CreateUserController().handle)
 router.post("/session", new AuthUserController().handle)
 router.get("/me", isAuthenticated, new DetailUserController().handle)
+
+// Rotas de Categoria
+router.post("/category", isAuthenticated, new CreateCategoryController().handle)
+router.get("/category", isAuthenticated, new ListCategoryController().handle)
 
 export { router };
