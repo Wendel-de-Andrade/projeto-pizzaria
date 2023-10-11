feda2c5bdaee62f61d310aa522faf669db9407b5
Wendel de Andrade <shindagami@gmail.com>
2023-10-11 02:29:19 -0300

    Inserida função de adicionar e remover itens na Order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -10,6 +10,8 @@ import { CreateProductController } from "./controllers/product/CreateProductCont
 import { ListByCategoryController } from "./controllers/product/ListByCaategoryController";
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+import { AddItemController } from "./controllers/order/AddItemController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import uploadConfig from "./config/multer";
@@ -34,5 +36,7 @@ router.get("/category/product", isAuthenticated, new ListByCategoryController().
 // Rotas de Pedidos (Order)
 router.post("/order", isAuthenticated, new CreateOrderController().handle)
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle)
+router.post("/order/add", isAuthenticated, new AddItemController().handle)
+router.delete("/order/remove", isAuthenticated, new RemoveItemController().handle)
 
 export { router };
