0a112f0503c78e2806d9b7737dbd4f03a60b9bb7
Wendel de Andrade <shindagami@gmail.com>
2023-10-12 18:32:53 -0300

    Listar orders prontas

---
+++ backend/src/controllers/order/ListOrderController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { ListOrderService } from "../../services/order/ListOrderService";
+
+class ListOrderController {
+    async handle(req: Request, res: Response) {
+
+        const listOrderServide = new ListOrderService();
+
+        const order = await listOrderServide.execute();
+
+        return res.json(order);
+
+    }
+}
+
+export { ListOrderController };
