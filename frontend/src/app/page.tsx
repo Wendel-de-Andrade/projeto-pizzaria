68567672eb9cd25d6d8fb28036bebbf0441de912
Wendel de Andrade <shindagami@gmail.com>
2023-10-13 00:43:27 -0300

    criação do document.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,19 @@
+import Head from "next/head"
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Pizzaria do Wendel - Faça seu Login",
+  description: "Sistema de pizzaria feita para estudo",
+}
+
 export default function Home() {
   return (
+    <>
     <div>
-      <h1>Pizzaria</h1>
+      <h1>
+        Pizzaria Braba
+      </h1>
     </div>
+    </>
   )
 }
