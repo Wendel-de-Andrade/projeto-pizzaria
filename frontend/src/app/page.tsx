cd7c76990a058018076e30d2f48278df6f3f7809
Wendel de Andrade <shindagami@gmail.com>
2023-10-14 01:30:15 -0300

    Criando pagina de Login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,11 @@
-import Head from "next/head"
 import type { Metadata } from 'next'
+import Image from 'next/image'
+import styles from "../../styles/page.module.scss"
+
+import { Input } from '../components/ui/input'
+import { Button } from '../components/ui/button'
+
+import logoImg from "../../public/logo.svg"
 
 export const metadata: Metadata = {
   title: "Pizzaria do Wendel - Faça seu Login",
@@ -9,11 +15,32 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-    <div>
-      <h1>
-        Pizzaria Braba
-      </h1>
-    </div>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Sujeito Pizzaria"></Image>
+
+        <div className={styles.login}>
+          <form>
+            <Input
+              placeholder="Digite seu email"
+              type="text"
+            />
+            <Input
+              placeholder="Sua senha"
+              type="password"
+            />
+            <Button
+              type="submit"
+              loading={false}
+            >
+              Acessar
+            </Button>
+
+          </form>
+
+          <a className={styles.text}>Não possui acesso? Cadastre-se!</a>
+
+        </div>
+      </div>
     </>
   )
 }
---
+++ frontend/src/components/ui/button/index.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, ButtonHTMLAttributes } from "react";
+import { FaSpinner } from "react-icons/fa"
+
+import styles from "./styles.module.scss"
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    loading?: boolean,
+    children: ReactNode,
+
+}
+
+export function Button({ loading, children, ...rest }: ButtonProps) {
+    return (
+        <button
+            className={styles.button}
+            disabled={loading}
+            {...rest}
+        >
+            {loading ? (
+                <FaSpinner color="#FFF" size={16} />
+            ) : (
+                <a className={styles.buttonName}>
+                    {children}
+                </a>
+            )}
+
+
+        </button>
+    )
+}
