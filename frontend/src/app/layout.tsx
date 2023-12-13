305765222f9f2ad237b25469af767c03b199f2bf
Wendel de Andrade <shindagami@gmail.com>
2023-12-12 23:49:40 -0300

    Context criado

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "../../styles/globals.scss"
 import { Inter } from 'next/font/google'
 
+import { AuthProvider } from "../contexts/AuthContext"
+
 const inter = Inter({ subsets: ['latin'] })
 
 
@@ -11,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </AuthProvider>
   )
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useContext, FormEvent, useState } from 'react'
+
 import type { Metadata } from 'next'
 import Image from 'next/image'
 import styles from "../../styles/page.module.scss"
@@ -5,28 +8,52 @@ import styles from "../../styles/page.module.scss"
 import { Input } from '../components/ui/input'
 import { Button } from '../components/ui/button'
 
+import { AuthContext } from '../contexts/AuthContext'
+
 import logoImg from "../../public/logo.svg"
 
-export const metadata: Metadata = {
-  title: "Pizzaria do Wendel - Faça seu Login",
-  description: "Sistema de pizzaria feita para estudo",
-}
+// export const metadata: Metadata = {
+//   title: "Pizzaria do Wendel - Faça seu Login",
+//   description: "Sistema de pizzaria feita para estudo",
+// }
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
+
   return (
     <>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Sujeito Pizzaria"></Image>
 
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input
               placeholder="Digite seu email"
               type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value) }
             />
             <Input
               placeholder="Sua senha"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value) }
             />
             <Button
               type="submit"
